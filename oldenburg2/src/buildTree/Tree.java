a011ce7ac2a962ea4d3cab8fa85dee9232649804
garynaruto <garynaruto@gmail.com>
2016-09-15 03:38:43 +0800

    cluster

--- oldenburg2/src/buildTree/Tree.java
+++ oldenburg2/src/buildTree/Tree.java
@@ -1,93 +1,111 @@
 package buildTree;
+
 import java.io.PrintWriter;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tree<T> implements Serializable{
+public class Tree<T> implements Serializable {
 	public Node<T> root;
-    public Tree(T rootData){
-        root = new Node<T>();
-        root.vertex = rootData;
-    }
-    public Tree(){
-        root = new Node<T>();
-        root.children = new ArrayList<Node<T>>();
-    }
-    public static class Node<T> implements Serializable{
-    	public T vertex;
-    	public Node<T> parent;
-    	public List<Node<T>> children;
-    	public List<Edge> edges;
-    	public List<Node<T>[]> skyLinePath;
-    	
-    	public Node(){
-    		children = new ArrayList<Node<T>>();
-    		edges = new ArrayList<Edge>();
-    		skyLinePath = new ArrayList<Node<T>[]>();
-    	}
-    	public void printTree(int count){
-    		for(int i=0; i<count; i++){
-    			System.out.print("  ");
-    		}
-    		System.out.print("."+vertex+"\n");
-    		count++;
+
+	public Tree(T rootData) {
+		root = new Node<T>();
+		root.vertex = rootData;
+	}
+
+	public Tree() {
+		root = new Node<T>();
+		root.children = new ArrayList<Node<T>>();
+	}
+
+	public static class Node<T> implements Serializable {
+		public T vertex;
+		public Node<T> parent;
+		public List<Node<T>> children;
+		public List<Edge> edges;
+		public List<Node<T>[]> skyLinePath;
+
+		public Node() {
+			children = new ArrayList<Node<T>>();
+			edges = new ArrayList<Edge>();
+			skyLinePath = new ArrayList<Node<T>[]>();
+		}
+
+		public void printTree(int count) {
+			for (int i = 0; i < count; i++) {
+				System.out.print("  ");
+			}
+			System.out.print("." + vertex + "\n");
+			count++;
+			for (Node<T> t : children) {
+				t.printTree(count);
+			}
+
+			for (Edge e : edges) {
+				for (int i = 0; i < count; i++) {
+					System.out.print("  ");
+				}
+				System.out.print("> " + e + "\n");
+			}
+			for (Node<T>[] e : skyLinePath) {
+				for (int i = 0; i < count; i++) {
+					System.out.print("  ");
+				}
+				System.out.print("-");
+				for (Node<T> t : e) {
+					System.out.print(t.vertex + ">");
+				}
+				System.out.println();
+			}
+			count--;
+			return;
+		}
+
+		public void writeTree(PrintWriter writer, int count) {
+			for (int i = 0; i < count; i++) {
+				writer.print("  ");
+			}
+			writer.println("." + vertex);
+			count++;
+			for (Node<T> t : children) {
+				t.writeTree(writer, count);
+			}
+
+			for (Edge e : edges) {
+				for (int i = 0; i < count; i++) {
+					writer.print("  ");
+				}
+				writer.println("> " + e);
+			}
+			for (Node<T>[] e : skyLinePath) {
+				for (int i = 0; i < count; i++) {
+					writer.print("  ");
+				}
+				writer.print("-");
+				for (Node<T> t : e) {
+					writer.print(t.vertex + ">");
+				}
+				writer.println();
+			}
+			count--;
+			return;
+		}
+
+		public void getContainVerte(Node<Vertex> n, List<Vertex> v){
+    		
     		for(Node<T> t : children){
-    			t.printTree(count);
+    			if(!t.vertex.equals(n.vertex)){
+        			v.add((Vertex)t.vertex);
+    			}
+    			t.getContainVerte(n,v);
     		}
     		
-    		for(Edge e : edges){
-    			for(int i=0; i<count; i++){
-        			System.out.print("  ");
-        		}
-        		System.out.print("> "+e+"\n");
-    		}
-    		for(Node<T>[] e : skyLinePath){
-    			for(int i=0; i<count; i++){
-        			System.out.print("  ");
-        		}
-    			System.out.print("-");
-    			for(Node<T> t : e){
-    				System.out.print(t.vertex+">");
-    			}
-    			System.out.println();
-    		}
-    		count--;
-    		return ;
+    		return;
     	}
-    	public void writeTree(PrintWriter writer, int count){
-    		for(int i=0; i<count; i++){
-    			 writer.print("  ");
-    		}
-    		writer.println("."+vertex);
-    		count++;
-    		for(Node<T> t : children){
-    			t.writeTree(writer, count);
-    		}
-    		
-    		for(Edge e : edges){
-    			for(int i=0; i<count; i++){
-    				writer.print("  ");
-        		}
-    			writer.println("> "+e);
-    		}
-    		for(Node<T>[] e : skyLinePath){
-    			for(int i=0; i<count; i++){
-    				writer.print("  ");
-        		}
-    			writer.print("-");
-    			for(Node<T> t : e){
-    				writer.print(t.vertex+">");
-    			}
-    			writer.println();
-    		}
-    		count--;
-    		return ;
-    	}
-    	
-    	public boolean isChildren(){
-        	return !this.children.isEmpty();
-        }
-    }
-    
+
+		public boolean isChildren() {
+			return !this.children.isEmpty();
+		}
+	}
+
 }
--- oldenburg2/src/search/qure.java
+++ oldenburg2/src/search/qure.java
@@ -1,29 +1,47 @@
-package buildTree;
+package search;
 
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import SkyPath.SkyNode;
 import SkyPath.SkyPath;
+import buildTree.Edge;
+import buildTree.Tree;
+import buildTree.Vertex;
+import buildTree.Tree.Node;
 
 public class qure {
+	public static Tree<Vertex> graphTree;
+	public static List<Edge> edgeList;
+	
 	public static void main(String args[]) throws Exception {
 		// Object deserialization 物件反序列化
 		/* graphTree */
-		Tree<Vertex> graphTree;
 		FileInputStream fis = new FileInputStream("./data/SerializationGraphTree");
 		ObjectInputStream ois = new ObjectInputStream(fis);
 		graphTree = (Tree<Vertex>) ois.readObject();
 		ois.close();
 		/* edgeList */
-		List<Edge> edgeList;
 		fis = new FileInputStream("./data/SerializationEdgeList");
 		ois = new ObjectInputStream(fis);
 		edgeList = (List<Edge>) ois.readObject();
 		ois.close();
 		
 		
+		List<Vertex> l = new ArrayList<Vertex>();
+		
+		Node<Vertex> n =graphTree.root.children.get(0);
+		n.getContainVerte(n, l);
+		
+		
+		System.out.println(">"+n.vertex.vertexID);
+		for(Vertex v :l){
+			System.out.println(v.vertexID);
+		}
+		
+		/*
 		int[] start ={2938};
 		int[] end ={3152};
 		SkyPath skyPath = new SkyPath();
@@ -37,11 +55,13 @@ public class qure {
 			for(Object a : ans){
 				System.out.println(a);
 			}
-		}
-		
-		
+		}*/
 	}
 	public static List<Vertex[]> findallpath(){
-		List<Vertex[]> out = new ArrayList<>
+		List<Vertex[]> out = new ArrayList<>();
+		
+		
+		
+		return out;
 	}
 }
